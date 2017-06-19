2e3a36a63e86885e2b6ce5e89d94b779eefebb5c
MI MUKIT <imukit007@gmail.com>
2017-06-19 21:36:33 +0600

    Add App component & youtube search func

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/SearchBar';
+import YTSearch from 'youtube-api-search';
 
-const App = () => {
-  return <div>Hello World!</div>
+const YT_API = 'AIzaSyDqL_re6cE8YhtNr_O7GvX1SX3aQo1clyg';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: YT_API, term: 'coc'}, (videos) => {
+      this.setState({ videos });
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+
 }
 
-ReactDom.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
