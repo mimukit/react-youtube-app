ae5969bffb87e2e07c564b00f7235618f3d43541
MI MUKIT <imukit007@gmail.com>
2017-06-21 16:42:46 +0600

    Add NavBar to Index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import _ from 'lodash';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
 import VideoPlayer from './components/VideoPlayer'
+import NavBar from './components/NavBar'
 
 
 const YT_API = 'AIzaSyDqL_re6cE8YhtNr_O7GvX1SX3aQo1clyg';
@@ -37,17 +38,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar
-          onChange={(searchTerm) => {this.videoSearch(searchTerm)}} />
-        <VideoPlayer video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={(selectedVideo) => {this.setState({selectedVideo})}}
-          videos={this.state.videos}
-        />
+        <NavBar siteTitle='React Youtube App' />
+        <div className="container">
+          <SearchBar
+            onChange={(searchTerm) => {this.videoSearch(searchTerm)}} />
+          <VideoPlayer video={this.state.selectedVideo} />
+          <VideoList
+            onVideoSelect={(selectedVideo) => {this.setState({selectedVideo})}}
+            videos={this.state.videos}
+            />
+        </div>
       </div>
     );
   }
 
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.app'));
